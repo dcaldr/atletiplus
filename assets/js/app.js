93b9fcbb746622965e7cc8042ca6b687e9fffca8
Dan Caldr <cald01@vse.cz>
2025-01-01 18:02:42 +0100

    fix: small steps

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+/**
+ * Pole všech dostupných kroků
+* @type {HTMLElement[]}
+ */
 let steps = [];
 let currentStep = 0;
 
@@ -6,14 +10,24 @@ function initializeSteps() {
     currentStep = 0;
 }
 
+/**
+ * Přepne krok (tj. zobrazí právě ten jeden a ostatní skryje)
+ * @param index {number} Index kroku, který se má zobrazit (0 = první krok)
+ * @returns {void} todo: vrátit něco?
+ */
 function showStep(index) {
+    /**todo: kontrola jestli číslo patří */
     steps.forEach((step, i) => {
         step.classList.toggle('d-none', i !== index);
     });
     currentStep = index;
 }
 
+/**
+ * Zpustí se až po načtení stránky
+ */
 document.addEventListener('DOMContentLoaded', () => {
+    //todo: přidat logiku pro nepovolený JS -- ie asi odstranit hlašku js je vypnutý (zatím není)
     initializeSteps();
-    showStep(0);
+    showStep(0); // pro jistotu
 });
